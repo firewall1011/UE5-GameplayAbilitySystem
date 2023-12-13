5da3e3eadbd448af77897f038586329892636275
Tiago Marino <tmsilva11@gmail.com>
2023-12-12 23:31:11 -0300

    Add Aura Base Character class

---
+++ Source/Aura/Aura.cpp
@@ -0,0 +1,6 @@
+// Copyright Tiago Marino Silva
+
+#include "Aura.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Aura, "Aura" );
---
+++ Source/Aura/Aura.h
@@ -0,0 +1,6 @@
+// Copyright Tiago Marino Silva
+
+#pragma once
+
+#include "CoreMinimal.h"
+
