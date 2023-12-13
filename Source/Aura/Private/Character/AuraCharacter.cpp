6ea0c7a8bac1d56e49088fba6588d69ece9e4df9
Tiago Marino <tmsilva11@gmail.com>
2023-12-12 23:53:11 -0300

    Create Aura Character class and BP

---
+++ Source/Aura/Private/Character/AuraCharacter.cpp
@@ -0,0 +1,16 @@
+﻿// Copyright Tiago Marino Silva
+
+
+#include "Character/AuraCharacter.h"
+
+
+AAuraCharacter::AAuraCharacter()
+{
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+void AAuraCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
---
+++ Source/Aura/Public/Character/AuraCharacter.h
@@ -0,0 +1,20 @@
+﻿// Copyright Tiago Marino Silva
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AuraCharacterBase.h"
+#include "AuraCharacter.generated.h"
+
+UCLASS()
+class AURA_API AAuraCharacter : public AAuraCharacterBase
+{
+	GENERATED_BODY()
+
+public:
+	AAuraCharacter();
+
+protected:
+	virtual void BeginPlay() override;
+};
+
