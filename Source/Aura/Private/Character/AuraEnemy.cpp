eb649a3b7dc663eb95ce4b7275f09e43e3561123
Tiago Marino <tmsilva11@gmail.com>
2023-12-12 23:59:03 -0300

    Create Goblin Spear Character class and BP

---
+++ Source/Aura/Private/Character/AuraEnemy.cpp
@@ -0,0 +1,16 @@
+﻿// Copyright Tiago Marino Silva
+
+
+#include "Character/AuraEnemy.h"
+
+
+AAuraEnemy::AAuraEnemy()
+{
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+void AAuraEnemy::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
---
+++ Source/Aura/Public/Character/AuraEnemy.h
@@ -0,0 +1,20 @@
+﻿// Copyright Tiago Marino Silva
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AuraCharacterBase.h"
+#include "AuraEnemy.generated.h"
+
+UCLASS()
+class AURA_API AAuraEnemy : public AAuraCharacterBase
+{
+	GENERATED_BODY()
+
+public:
+	AAuraEnemy();
+
+protected:
+	virtual void BeginPlay() override;
+};
+
