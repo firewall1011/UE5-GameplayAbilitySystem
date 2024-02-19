64c26bf5a6e549a7d0d45f3f9d1fb86e438314fb
Tiago Marino Silva <tmsilva11@gmail.com>
2024-02-19 17:16:43 -0300

    Add AuraHUD

---
+++ Source/Aura/Private/UI/AuraHUD.cpp
@@ -0,0 +1,13 @@
+﻿// Copyright Tiago Marino Silva
+
+
+#include "UI/AuraHUD.h"
+#include "UI/Widget/AuraUserWidget.h"
+
+void AAuraHUD::BeginPlay()
+{
+	Super::BeginPlay();
+
+	UUserWidget* Widget = CreateWidget<UUserWidget>(GetWorld(), OverlayWidgetClass);
+	Widget->AddToViewport();
+}
