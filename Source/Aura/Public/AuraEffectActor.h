bc7d3408c613c072e14f53b2a75bceed9e728265
Tiago Marino Silva <tmsilva11@gmail.com>
2024-02-22 12:27:47 -0300

    Create Duration based Crystal buff effect

--- Source/Aura/Public/AuraEffectActor.h
+++ Source/Aura/Public/AuraEffectActor.h
@@ -24,4 +24,7 @@ protected:
 	
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Applied Effects")
 	TSubclassOf<UGameplayEffect> InstantGameplayEffect;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Applied Effects")
+	TSubclassOf<UGameplayEffect> DurationGameplayEffect;
 };
